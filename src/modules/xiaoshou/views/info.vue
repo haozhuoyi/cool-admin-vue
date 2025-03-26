a5a7ad5c5cd1fbc7210bfabaa5370bdb08f813c9
Hao Zhuoyi <72720742+haozhuoyi@users.noreply.github.com>
2025-03-26 13:44:15 +0800

    销售模块

---
+++ src/modules/xiaoshou/views/info.vue
@@ -0,0 +1,123 @@
+<template>
+	<cl-crud ref="Crud">
+		<cl-row>
+			<!-- 刷新按钮 -->
+			<cl-refresh-btn />
+			<!-- 新增按钮 -->
+			<cl-add-btn />
+			<!-- 删除按钮 -->
+			<cl-multi-delete-btn />
+			<cl-flex1 />
+			<!-- 条件搜索 -->
+			<cl-search ref="Search" />
+		</cl-row>
+
+		<cl-row>
+			<!-- 数据表格 -->
+			<cl-table ref="Table" />
+		</cl-row>
+
+		<cl-row>
+			<cl-flex1 />
+			<!-- 分页控件 -->
+			<cl-pagination />
+		</cl-row>
+
+		<!-- 新增、编辑 -->
+		<cl-upsert ref="Upsert" />
+	</cl-crud>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+	name: "xiaoshou-info",
+});
+
+import { useCrud, useTable, useUpsert, useSearch } from "@cool-vue/crud";
+import { useCool } from "/@/cool";
+import { useI18n } from "vue-i18n";
+
+const { service } = useCool();
+const { t } = useI18n();
+
+// cl-upsert
+const Upsert = useUpsert({
+	items: [
+		{
+			label: t("名称"),
+			prop: "name",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+		{
+			label: t("选择关联渠道"),
+			prop: "qudaoIds",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+		{
+			label: t("选择名下租户"),
+			prop: "zuhuIds",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+		{
+			label: t("联系方式"),
+			prop: "phone",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+		{
+			label: t("备注"),
+			prop: "note",
+			component: { name: "el-input", props: { clearable: true } },
+			span: 12,
+		},
+	],
+});
+
+// cl-table
+const Table = useTable({
+	columns: [
+		{ type: "selection" },
+		{ label: t("名称"), prop: "name", minWidth: 120 },
+		{ label: t("关联渠道"), prop: "qudaoIds", minWidth: 120 },
+		{ label: t("名下租户"), prop: "zuhuIds", minWidth: 120 },
+		{ label: t("联系方式"), prop: "phone", minWidth: 120 },
+		{ label: t("备注"), prop: "note", minWidth: 120 },
+		{
+			label: t("创建时间"),
+			prop: "createTime",
+			minWidth: 170,
+			sortable: "desc",
+			component: { name: "cl-date-text" },
+		},
+		{
+			label: t("更新时间"),
+			prop: "updateTime",
+			minWidth: 170,
+			sortable: "custom",
+			component: { name: "cl-date-text" },
+		},
+		{ type: "op", buttons: ["edit", "delete"] },
+	],
+});
+
+// cl-search
+const Search = useSearch();
+
+// cl-crud
+const Crud = useCrud(
+	{
+		service: service.xiaoshou.info,
+	},
+	(app) => {
+		app.refresh();
+	},
+);
+
+// 刷新
+function refresh(params?: any) {
+	Crud.value?.refresh(params);
+}
+</script>
